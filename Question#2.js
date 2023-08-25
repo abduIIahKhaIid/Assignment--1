ffe1b9b8ba6ecacb1d522272aa7b765d66878616
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-25 23:05:55 +0500

    First Commit

--- Question#2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let Name = "Abdullah Khalid";
-let message = 'Hello  ${name}, would you like to learn some Python today?';
-console.log(message);
---
+++ Question#4.ts
@@ -0,0 +1,4 @@
+let quote: string = "The greatest glory in living lies not in never falling, but in rising every time we fall.";
+let author: string = "Nelson Mandela";
+
+console.log(`"${quote}" - ${author}`);
