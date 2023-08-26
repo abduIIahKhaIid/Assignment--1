7bf25b74a837b3dfea47077d00ce4e8d19a14cb6
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-27 00:19:52 +0500

    first commit

--- Question_21.ts
+++ Question_21.ts
@@ -4,3 +4,24 @@
   Write a program that creates Objects containing these items.
 */
 
+let obj: { title: string, author: string, year: number }[] = [
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        year: 1949
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925
+    }
+]
+
+console.log("Array of object", obj);
+
+export { }
