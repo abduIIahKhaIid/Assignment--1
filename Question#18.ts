2dc1ae5b9f965fcc20f6882c15e7272ee94d2ad1
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-26 16:49:17 +0500

    first commit

---
+++ Question#18.ts
@@ -0,0 +1,6 @@
+/*
+  Question#18
+  Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+  create a message that reveals your favorite number. Print that message.
+*/
+
