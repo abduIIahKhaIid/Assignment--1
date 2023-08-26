945fb479dc781b1e1608590582599ccb664f5a35
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-27 00:09:35 +0500

    first commit

--- Question_03.ts
+++ Question_03.ts
@@ -3,8 +3,27 @@
   Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 */
 
-let Name: string = "Abdullah Khalid";
+let Name: string = "Abdullah khalid";
 console.log(`Lower Case: ${Name.toLocaleLowerCase()}`);
 console.log(`Upper Case ${Name.toUpperCase()}`);
+let titleCase: string = Name.charAt(0).toUpperCase();
+let check: boolean = false;
+for (let i = 1; i < Name.length; i++) {
+  if (Name[i] == " ") {
+    check = true;
+    titleCase += Name[i];
+    continue;
+  }
+  if (check) {
+    check=false;
+    titleCase += Name[i].toUpperCase();
+  }
+  else {
+    titleCase += Name[i].toLowerCase();
+  }
+
+}
+
+console.log("Title Case: ", titleCase);
 
 export { }
---
+++ Question_21.ts
@@ -0,0 +1,6 @@
+/*
+  Question#21
+  They think of something you could store in a TypeScript Object. 
+  Write a program that creates Objects containing these items.
+*/
+
