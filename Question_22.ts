ce5e54ef8ec26b0d81c2026e4a7561fb7fd70b1f
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-28 23:14:51 +0500

    first commit

--- Question_22.ts
+++ Question_22.ts
@@ -4,3 +4,15 @@
   try to make one happen. Change an index in one of your programs to produce an index error. 
   Make sure you correct the error before closing the program.
 */
+
+let obj: { name: string, model: number }[] = [
+    { name: "Honda", model: 2013 },
+    { name: "Cultus", model: 2022 },
+    { name: "Corolla", model: 2002 }
+]
+
+//Index Error
+console.log(obj[3]);
+
+//Error Correction
+console.log(obj[2]);
