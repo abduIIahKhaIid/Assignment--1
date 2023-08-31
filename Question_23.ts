26eaa3644d7bd49d2d59aadd81b83df01481de3a
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-31 15:07:10 +0500

    first commit

--- Question_23.ts
+++ Question_23.ts
@@ -1,9 +1,27 @@
 /*
   Question#23
-  Intentional Error: If you haven’t received an array index error in one of your programs yet, 
-  try to make one happen. Change an index in one of your programs to produce an index error. 
-  Make sure you correct the error before closing the program.
+  IConditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for 
+  the results of each test. Your code should look something like this:
+  let car = 'subaru';
+  console.log("Is car == 'subaru'? I predict True.")
+  console.log(car == 'subaru')
+  • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+  • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
 */
+let car = "subaru";
+let rollNo: number = 20;
+let studentName: string = 'Abdullah';
 
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
 
-export { }
+console.log("Is rollNo == 20 ? I predict True.");
+console.log(rollNo == 20);
+
+console.log("Is studentName == 'Abdullah' ? I predict True.");
+console.log(studentName == 'Abdullah');
+
+console.log("Is rollNo less than 20 ? I predict true");
+console.log(rollNo < 20);
+
+export { };
