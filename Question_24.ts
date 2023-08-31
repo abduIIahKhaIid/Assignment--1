1878521e993d980ef7a395bdf24639f26bcfff43
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-31 22:39:50 +0500

    first commit

--- Question_24.ts
+++ Question_24.ts
@@ -1,4 +1,5 @@
 /*
+  Question#24
   More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to 
   try more comparisons, write more tests. Have at least one True and one False result for each of the following:
 */
