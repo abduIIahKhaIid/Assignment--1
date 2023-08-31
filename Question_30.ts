7f41d768932d3d498cb04098bdae3893237b9b6a
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-08-31 23:43:54 +0500

    first commit

--- Question_30.ts
+++ Question_30.ts
@@ -1,5 +1,5 @@
 /*
-  Question#29
+  Question#30
   Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code 
   that will print a greeting to each user after they log in to a website. Loop through the array, and print a 
   greeting to each user:
