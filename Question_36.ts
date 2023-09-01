a1fd09dc64e8166921b9fe5813f183d1828c58e7
abduIIahKhaIid <abdullahakhalid6@gmail.com>
2023-09-01 19:59:14 +0500

    first commit

---
+++ Question_36.ts
@@ -0,0 +1,14 @@
+/*
+  Question#36
+  T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that 
+  should be printed on the shirt. The function should print a sentence summarizing the size of the shirt 
+  and the message printed on it. Call the function.
+*/
+
+function make_shirt(size: number, text: string) {
+    console.log(`The shirt size is ${size}, and the text of a message: ${text}`);
+}
+
+make_shirt(20, 'I love Pakistan');
+
+export { }
